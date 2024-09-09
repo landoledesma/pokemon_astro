e07aac60299273ffa0700ac9cd5714c2347bbe27
orlando castellanos <lando_ledesma@outlook.com>
2024-09-09 10:32:09 -0600

    add icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 
+import icon from 'astro-icon';
+
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), icon()]
 });
